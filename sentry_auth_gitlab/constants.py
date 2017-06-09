d96fa3e372101ff0988690768af4081afe922996
SkyLothar <allothar@gmail.com>
2017-06-09 18:41:21 +0800

    read scope from settings

--- sentry_auth_gitlab/constants.py
+++ sentry_auth_gitlab/constants.py
@@ -7,10 +7,9 @@ CLIENT_SECRET = getattr(settings, 'GITLAB_APP_SECRET', None)
 
 BASE_DOMAIN = getattr(settings, 'GITLAB_BASE_DOMAIN', None)
 SCHEME = getattr(settings, 'GITLAB_HTTP_SCHEME', 'https')
-API_VERSION = getattr(settings, 'GITLAB_API_VERSION', 3)
+API_VERSION = getattr(settings, 'GITLAB_API_VERSION', 4)
+SCOPE = getattr(settings, 'GITLAB_AUTH_SCOPE', 'api')
 
 ACCESS_TOKEN_URL = '{0}://{1}/oauth/token'.format(SCHEME, BASE_DOMAIN)
 AUTHORIZE_URL = '{0}://{1}/oauth/authorize'.format(SCHEME, BASE_DOMAIN)
 API_BASE_URL = '{0}://{1}/api/v{2}'.format(SCHEME, BASE_DOMAIN, API_VERSION)
-
-SCOPE = 'api'
